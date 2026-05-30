87df7cd41b5c6b82ce465d61e3370383d902e09d
Adam Wakeling <adam@notionpictures.net>
2026-05-30 22:58:45 +0200

    Remove factory_bot from application config

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,6 @@ module Inga
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
-    config.factory_bot.definition_file_paths = ["test/factories"]
 
     # Use MiniMagick for image processing
     config.active_storage.variant_processor = :mini_magick
